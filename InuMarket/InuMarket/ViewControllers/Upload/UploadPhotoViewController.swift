160377083d06d68d2e5aa7ed171c660f2d07b365
동균 <hdg9455@naver.com>
2018-09-19 02:31:36 +0900

    긴급 수정  - 카메라 촬영 업로드 사진 용량 줄임  테스트후 해상도 조절할 것

--- InuMarket/InuMarket/ViewControllers/Upload/UploadPhotoViewController.swift
+++ InuMarket/InuMarket/ViewControllers/Upload/UploadPhotoViewController.swift
@@ -100,7 +100,8 @@ class UploadPhotoViewController: UIViewController, UIImagePickerControllerDelega
                     self.view.makeToast("7장이 최대입니다.")
                 }
             }else{
-                imageArray.append(originalImage)
+                self.resizeImage(image: originalImage, targetSize: CGSize(width: 200.0, height: 200.0))
+//                imageArray.append(originalImage)
                 OperationQueue.main.addOperation {
                     self.photoCollection.reloadData()
                 }
@@ -211,7 +212,31 @@ class UploadPhotoViewController: UIViewController, UIImagePickerControllerDelega
         }
     }
     
-    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.imageArray.append(newImage!)
+        return newImage!
+    }
     
     func initializing() {
         // button initializing
